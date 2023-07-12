b7d576bb43c57c1bb1b6e135d9f6cec9817b44f3
Bryan Varner <1652015+bvarner@users.noreply.github.com>
2023-07-12 00:42:56 -0400

    Preparation for Hardware PWM.
    feat: add hardware pwm device references to the driver for valve / blower control
    feat: Setup the 1 Min pulse timer to fire from the driver. If the driver hangs, the SBC hiccups, etc. this will stop pulsing the 1Min line.
    cleanup: remove softPWM for blower / valve control. Removed GPIO mappings.

--- spcd.c
+++ spcd.c
@@ -32,6 +32,7 @@
 #include <linux/platform_device.h>
 #include <linux/module.h>
 #include <linux/poll.h>
+#include <linux/pwm.h>
 #include <linux/uaccess.h>
 #include <linux/hrtimer.h>
 // Added to support sysfs_emit, being pulled in from newer kernel releases.
@@ -40,47 +41,42 @@
 #include <linux/slab.h>
 
 struct spcd_data {
-    struct device *dev;
-    struct gpio_desc *gpio_in_12v_status;
-    int irq_12v_status;
+    struct device       *dev;
+    struct gpio_desc    *gpio_in_12v_status;
+    int                 irq_12v_status;
 
-    struct gpio_desc *gpio_in_valve_open;
-    int irq_valve_open;
+    struct gpio_desc    *gpio_in_valve_open;
+    int                 irq_valve_open;
 
-    struct gpio_desc *gpio_in_overpressure;
-    int irq_overpressure;
+    struct gpio_desc    *gpio_in_overpressure;
+    int                 irq_overpressure;
 
-    struct gpio_desc *gpio_in_stuckon;
-    int irq_stuckon;
+    struct gpio_desc    *gpio_in_stuckon;
+    int                 irq_stuckon;
 
-    struct gpio_desc *gpio_in_mode;
-    int irq_mode;
+    struct gpio_desc    *gpio_in_mode;
+    int                 irq_mode;
 
-
-    struct gpio_desc *gpio_out_pwr_hold; // Unused
-    struct gpio_desc *gpio_out_wdt_alert;
-
-    struct gpio_desc *gpio_out_buzzer_low;
-    struct gpio_descs *gpio_out_buzzer_medium;
-    struct gpio_descs *gpio_out_buzzer_high;
-    struct gpio_desc *gpio_out_blower_stat;
-    struct gpio_desc *gpio_out_1min;
-    struct gpio_desc *gpio_out_blower_control;
-    struct gpio_desc *gpio_out_valve_control;
+    struct pwm_device   *pwmd_blower;
+    struct pwm_device   *pwmd_valve;
 
 
-    ktime_t blower_period;
-    ktime_t blower_duty_on;
-    ktime_t blower_duty_off;
-    u8 blower_duty_state;
-    struct hrtimer blower_timer;
+    struct gpio_desc    *gpio_out_pwr_hold; // Unused
+    struct gpio_desc    *gpio_out_wdt_alert;
 
+    struct gpio_desc    *gpio_out_buzzer_low;
+    struct gpio_descs   *gpio_out_buzzer_medium;
+    struct gpio_descs   *gpio_out_buzzer_high;
+    struct gpio_desc    *gpio_out_blower_stat;
+    struct gpio_desc    *gpio_out_1min;
 
-    ktime_t valve_period;
-    ktime_t valve_duty_on;
-    ktime_t valve_duty_off;
-    u8 valve_duty_state;
-    struct hrtimer valve_timer;
+    struct pwm_state    blower_state;
+    struct pwm_state    valve_state;
+    u8                  buzzer_enable;
+
+    struct hrtimer      pulse_timer;
+    u8                  pulse_value;
+    ktime_t             pulse_period;
 };
 
 
@@ -102,231 +98,39 @@ int sysfs_emit(char *buf, const char *fmt, ...) {
     return len;
 }
 
+// Pulse Timer Callback
+enum hrtimer_restart pulse_timer_callback(struct hrtimer *timer) {
+    pr_debug(" %s\n", __FUNCTION__);
+    struct spcd_data *spcd = container_of(timer, struct spcd_data, pulse_timer);
 
-
-// Blower PWM Callback
-enum hrtimer_restart blower_timer_callback(struct hrtimer *timer) {
-    struct spcd_data *spcd = container_of(timer, struct spcd_data, blower_timer);
-
-    // Swap from on to off.
-    spcd->blower_duty_state = spcd->blower_duty_state ^ 1;
-    gpiod_set_value(spcd->gpio_out_blower_control, spcd->blower_duty_state);
-
-    // If the pins are off, wait for the time they should be off.
-    // If the pins are on, wait for the time they should be on.
-    hrtimer_forward_now(&spcd->blower_timer, spcd->blower_duty_state == 0 ? spcd->blower_duty_off : spcd->blower_duty_on);
+    // toggle pin state.
+    spcd->pulse_value = spcd->pulse_value ^ 1;
+    gpiod_set_value_cansleep(spcd->gpio_out_1min, spcd->pulse_value);
+    hrtimer_forward_now(&spcd->pulse_timer, spcd->pulse_period);
 
     return HRTIMER_RESTART;
 }
 
 
-// Valve PWM Callback
-enum hrtimer_restart valve_timer_callback(struct hrtimer *timer) {
-    struct spcd_data *spcd = container_of(timer, struct spcd_data, valve_timer);
 
-    spcd->valve_duty_state = spcd->valve_duty_state ^ 1;
-    gpiod_set_value(spcd->gpio_out_valve_control, spcd->valve_duty_state);
+static int spcd_set_state(struct spcd_data *spcd) {
+    gpiod_set_value(spcd->gpio_out_buzzer_low, spcd->buzzer_enable);
+    gpiod_set_value(spcd->gpio_out_blower_stat, spcd->blower_state.enabled ? 1 : 0);
+//    gpiod_set_value(spcd->gpio_out_pwr_hold, 0); // TODO: Future. Currently DNP.
+//    gpiod_set_value(spcd->gpio_out_wdt_alert, 0); // TODO: Set this if we restart 'unclean'.
 
-    hrtimer_forward_now(&spcd->valve_timer, spcd->valve_duty_state == 0 ? spcd->valve_duty_off : spcd->valve_duty_on);
+    pwm_apply_state(spcd->pwmd_blower, &(spcd->blower_state));
+    pwm_apply_state(spcd->pwmd_valve, &(spcd->valve_state));
 
-    return HRTIMER_RESTART;
+    return 0;
+}
+
+static int spcd_read_state(struct spcd_data *spcd) {
+    // TODO: read the input lines and derive that into state on the spcd struct.
+    return 0;
 }
 
 
-static void spcd_blower_timer_update(struct spcd_data *spcd) {
-    pr_debug(" %s\n", __FUNCTION__);
-
-    // Clear any pending timers.
-    hrtimer_try_to_cancel(&spcd->blower_timer);
-
-    // Set the pins low. (blower OFF)
-    // TODO: Check if we can safely move this to the else block handling zero-ing out timers. If so, we can eliminate a 'hiccup'.
-    gpiod_set_value(spcd->gpio_out_blower_control, 0);
-    gpiod_set_value_cansleep(spcd->gpio_out_blower_stat, 0);
-
-    // Recalculate the blower duty.
-    if (spcd->blower_period > 0 && spcd->blower_duty_on > 0) {
-        pr_debug("   period and duty set.\n");
-
-        spcd->blower_duty_off = ktime_set(0, ktime_sub(spcd->blower_period, spcd->blower_duty_on));
-
-        pr_debug("  on:%lld    off:%lld\n", ktime_to_ns(spcd->blower_duty_on), ktime_to_ns(spcd->blower_duty_off));
-
-        // Set the pin states.
-        // We're going to turn the blower on.
-        gpiod_set_value_cansleep(spcd->gpio_out_blower_stat, 1);
-        pr_debug("  blower_stat on.\n");
-
-        // We're going to pulse this pin.
-        spcd->blower_duty_state = 1;
-        gpiod_set_value(spcd->gpio_out_blower_control, spcd->blower_duty_state);
-        pr_debug("  blower_control on.\n");
-
-        // If the duty is 100, blower_duty_off will be 0, and we don't need the timer.
-        if (ktime_to_ns(spcd->blower_duty_off) > 0) {
-            pr_debug("  ENABLING BLOWER PWM TIMER\n");
-            // Set a timer for the duty to be on.
-            hrtimer_forward_now(&spcd->blower_timer, spcd->blower_duty_on);
-            // fire the timer.
-            hrtimer_restart(&spcd->blower_timer);
-        } else {
-            pr_debug(" FULL DUTY. NO TIMER\n");
-        }
-    } else {
-        pr_debug("   period or duty zero. Leaving off.\n");
-        spcd->blower_duty_on = ktime_set(0, 0);
-        spcd->blower_duty_off = ktime_set(0, 0);
-    }
-
-    return;
-}
-
-
-static void spcd_valve_timer_update(struct spcd_data *spcd) {
-    pr_debug(" %s\n", __FUNCTION__);
-
-    // Clear any pending timers.
-    hrtimer_try_to_cancel(&spcd->valve_timer);
-
-    // Set the pins low. (valve OFF)
-    gpiod_set_value(spcd->gpio_out_valve_control, 0);
-
-    // Recalculate the valve duty.
-    if (spcd->valve_period > 0 && spcd->valve_duty_on > 0) {
-        pr_debug("   valve period and duty set.\n");
-
-        spcd->valve_duty_off = ktime_set(0, ktime_sub(spcd->valve_period, spcd->valve_duty_on));
-
-        pr_debug("  on:%lld    off:%lld\n", ktime_to_ns(spcd->valve_duty_on), ktime_to_ns(spcd->valve_duty_off));
-
-        // Set the pin states.
-        // We're going to pulse this pin.
-        spcd->valve_duty_state = 1;
-        gpiod_set_value(spcd->gpio_out_valve_control, spcd->valve_duty_state);
-        pr_debug("  valve_control on.\n");
-
-        // If the duty is 100, valve_duty_off will be 0, and we don't need the timer.
-        if (ktime_to_ns(spcd->valve_duty_off) > 0) {
-            pr_debug("  ENABLING VALVE PWM TIMER\n");
-            // Set a timer for the duty to be on.
-            hrtimer_forward_now(&spcd->valve_timer, spcd->valve_duty_on);
-            // fire the timer.
-            hrtimer_restart(&spcd->valve_timer);
-        } else {
-            pr_debug(" FULL DUTY. NO TIMER\n");
-        }
-    } else {
-        pr_debug("   period or duty zero. Leaving off.\n");
-        spcd->valve_duty_on = ktime_set(0, 0);
-        spcd->valve_duty_off = ktime_set(0, 0);
-    }
-
-    return;
-}
-
-
-/* -- sysfs attributes -- PWM -- */
-static ssize_t blower_duty_cycle_show(struct device *dev, struct device_attribute *attr, char *buf) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-
-    return sysfs_emit(buf, "%lld\n", ktime_to_ns(spcd->blower_duty_on));
-}
-
-static ssize_t blower_duty_cycle_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-    long dutyonnanos;
-    int err;
-
-    err = kstrtol(buf, 10, &dutyonnanos);
-    if (err) {
-        return err;
-    }
-
-    spcd->blower_duty_on = ktime_set(0, dutyonnanos);
-    spcd_blower_timer_update(spcd);
-
-    return count;
-}
-
-static DEVICE_ATTR_RW(blower_duty_cycle);
-
-
-
-static ssize_t blower_period_show(struct device *dev, struct device_attribute *attr, char *buf) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-
-    return sysfs_emit(buf, "%lld\n", ktime_to_ns(spcd->blower_period));
-}
-
-static ssize_t blower_period_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-    long periodnanos;
-    int err;
-
-    err = kstrtol(buf, 10, &periodnanos);
-    if (err) {
-        return err;
-    }
-
-    spcd->blower_period = ktime_set(0, periodnanos);
-    spcd_blower_timer_update(spcd);
-
-    return count;
-}
-
-static DEVICE_ATTR_RW(blower_period);
-
-
-
-static ssize_t valve_duty_cycle_show(struct device *dev, struct device_attribute *attr, char *buf) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-
-    return sysfs_emit(buf, "%lld\n", ktime_to_ns(spcd->valve_duty_on));
-}
-
-static ssize_t valve_duty_cycle_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-    long dutynanos;
-    int err;
-
-    err = kstrtol(buf, 10, &dutynanos);
-    if (err) {
-            return err;
-    }
-
-    spcd->valve_duty_on = ktime_set(0, dutynanos);
-    spcd_valve_timer_update(spcd);
-
-    return count;
-}
-
-static DEVICE_ATTR_RW(valve_duty_cycle);
-
-
-
-static ssize_t valve_period_show(struct device *dev, struct device_attribute *attr, char *buf) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-
-    return sysfs_emit(buf, "%lld\n", ktime_to_ns(spcd->valve_period));
-}
-
-static ssize_t valve_period_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-    struct spcd_data *spcd = dev_get_drvdata(dev);
-    long periodnanos;
-    int err;
-
-    err = kstrtol(buf, 10, &periodnanos);
-    if (err) {
-        return err;
-    }
-
-    spcd->valve_period = ktime_set(0, periodnanos);
-    spcd_valve_timer_update(spcd);
-
-    return count;
-}
-
-static DEVICE_ATTR_RW(valve_period);
 
 
 /* sysfs Attributes -- INPUTS */
@@ -583,10 +387,6 @@ static DEVICE_ATTR_RW(buzzer);
 
 
 static struct attribute *spcd_attrs[] = {
-        &dev_attr_blower_duty_cycle.attr,
-        &dev_attr_blower_period.attr,
-        &dev_attr_valve_duty_cycle.attr,
-        &dev_attr_valve_period.attr,
         &dev_attr_status_12v.attr,
         &dev_attr_valve_open.attr,
         &dev_attr_overpressure.attr,
@@ -661,6 +461,38 @@ static int spcd_probe(struct platform_device *pdev) {
         return -ENOMEM;
     }
     spcd_data->dev = dev;
+    spcd_data->pulse_value = 0;
+    spcd_data->pulse_period = ktime_set(30, 0);
+
+    hrtimer_init(&spcd_data->pulse_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL_PINNED_HARD);
+    spcd_data->pulse_timer.function = pulse_timer_callback;
+
+    // spcd_data->input_dirty = false;
+
+    // PWM devices from device tree bindings
+    spcd_data->pwmd_blower = devm_pwm_get(dev, "blower-pwm");
+    if (IS_ERR(spcd_data->pwmd_blower)) {
+        dev_err(dev, "failed to acquire blower PWM device. err=%ld\n", PTR_ERR(spcd_data->pwmd_blower));
+        return PTR_ERR(spcd_data->pwmd_blower);
+    }
+    pwm_init_state(spcd_data->pwmd_blower, &(spcd_data->blower_state));
+    spcd_data->blower_state.period = 100000000;
+    spcd_data->blower_state.polarity = PWM_POLARITY_NORMAL;
+    pwm_set_relative_duty_cycle(&(spcd_data->blower_state), 50, 100);
+    spcd_data->blower_state.enabled = false;
+
+
+    spcd_data->pwmd_valve = devm_pwm_get(dev, "valve-pwm");
+    if (IS_ERR(spcd_data->pwmd_valve)) {
+        dev_err(dev, "failed to acquire valve PWM device. err=%ld\n", PTR_ERR(spcd_data->pwmd_valve));
+        return PTR_ERR(spcd_data->pwmd_valve);
+    }
+    pwm_init_state(spcd_data->pwmd_valve, &(spcd_data->valve_state));
+    spcd_data->valve_state.period = 1000000000;
+    spcd_data->valve_state.polarity = PWM_POLARITY_NORMAL;
+    pwm_set_relative_duty_cycle(&(spcd_data->valve_state), 50, 100);
+    spcd_data->blower_state.enabled = false;
+
 
     // Setup Input GPIOS and IRQs
     spcd_data->gpio_in_12v_status = devm_gpiod_get(dev, "in-12v-status", GPIOD_IN);
@@ -737,19 +569,6 @@ static int spcd_probe(struct platform_device *pdev) {
         return PTR_ERR(spcd_data->gpio_out_wdt_alert);
     }
 
-    spcd_data->gpio_out_blower_control = devm_gpiod_get(dev, "out-blower-control", GPIOD_OUT_LOW);
-    if (IS_ERR(spcd_data->gpio_out_blower_control)) {
-        dev_err(dev, "failed to get out-blower-control-gpio: err=%ld\n", PTR_ERR(spcd_data->gpio_out_blower_control));
-        return PTR_ERR(spcd_data->gpio_out_blower_control);
-    }
-
-    spcd_data->gpio_out_valve_control = devm_gpiod_get(dev, "out-valve-control", GPIOD_OUT_LOW);
-    if (IS_ERR(spcd_data->gpio_out_valve_control)) {
-        dev_err(dev, "failed to get out-valve-control-gpio: err=%ld\n", PTR_ERR(spcd_data->gpio_out_valve_control));
-        return PTR_ERR(spcd_data->gpio_out_valve_control);
-    }
-
-
 
     // All outputs on the I2C expander are open-drain.
     spcd_data->gpio_out_buzzer_low = devm_gpiod_get(dev, "out-buzzer-low", GPIOD_OUT_LOW_OPEN_DRAIN);
@@ -778,31 +597,14 @@ static int spcd_probe(struct platform_device *pdev) {
         return PTR_ERR(spcd_data->gpio_out_1min);
     }
 
-    spcd_data->blower_period = ktime_set(0, 0);
-    spcd_data->blower_duty_on = ktime_set(0, 0);
-    spcd_data->blower_duty_off = ktime_set(0, 0);
-    spcd_data->blower_duty_state = 0;
-
-    spcd_data->valve_period = ktime_set(0, 0);
-    spcd_data->valve_duty_on = ktime_set(0, 0);
-    spcd_data->valve_duty_off = ktime_set(0, 0);
-    spcd_data->valve_duty_state = 0;
-
-    // TODO: Initial GPIO state tracking vars.
-
-    // Setup softPWM hardware timers.
-    hrtimer_init(&spcd_data->blower_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL_PINNED_HARD);
-    spcd_data->blower_timer.function = blower_timer_callback;
-
-    hrtimer_init(&spcd_data->valve_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL_PINNED_HARD);
-    spcd_data->valve_timer.function = valve_timer_callback;
-
+    // Setup initial GPIO states
+    spcd_data->buzzer_enable = 0;
 
     platform_set_drvdata(pdev, spcd_data);
 
-    // TODO sync state with a set / read.
-    spcd_blower_timer_update(spcd_data);
-    spcd_valve_timer_update(spcd_data);
+    // sync initial state
+    spcd_set_state(spcd_data);
+    // spcd_read_state(spcd_data);
 
 
     // Associate sysfs attribute groups.
@@ -857,6 +659,11 @@ static int spcd_probe(struct platform_device *pdev) {
         dev_err(&pdev->dev, "couldn't request irq %s %d: %d\n", "irq_mode", spcd_data->irq_mode, ret);
         return ret;
     }
+
+    // Startup the pulse timer.
+    hrtimer_try_to_cancel(&spcd_data->pulse_timer);
+    hrtimer_forward_now(&spcd_data->pulse_timer, spcd_data->pulse_period);
+    hrtimer_restart(&spcd_data->pulse_timer);
 
     // Fall through to success
     return ret;
